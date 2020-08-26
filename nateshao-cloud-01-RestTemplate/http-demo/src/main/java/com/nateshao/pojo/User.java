70f731af6eed008a31d7f34821af4dd077644e56
nateshao <shaotongjie@gmail.com>
2020-08-27 00:30:40 +0800

    初始化完成

--- nateshao-cloud-01-RestTemplate/http-demo/src/main/java/com/nateshao/pojo/User.java
+++ nateshao-cloud-01-RestTemplate/http-demo/src/main/java/com/nateshao/pojo/User.java
@@ -4,6 +4,14 @@ import lombok.Data;
 
 import java.util.Date;
 
+/**
+ * @date Created by 邵桐杰 on 2020/8/27 0:14
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ */
 @Data
 public class User {
     private Long id;
